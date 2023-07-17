50fdc0663d9189dcd09dbe2c370b1c56250d81e9
Greg Schwartz <greg.schwartz@northwestern.edu>
2023-07-17 17:36:38 -0500

    Update RF_model_script.m

--- RF_model_script.m
+++ RF_model_script.m
@@ -33,7 +33,7 @@ end
 
 %%
 point_location = [.25 .5];
-N_grid = 60;
+N_grid = 50;
 rangeX = [-1 1];
 rangeY = [-1 1];
 xvals = linspace(rangeX(1),rangeX(2),N_grid);
@@ -45,7 +45,7 @@ posterior_gauss_sigY = zeros(N_noise, N_radius);
 posterior_gauss_muX = zeros(N_noise, N_radius);
 posterior_gauss_muY = zeros(N_noise, N_radius);
 posterior_gauss_ang = zeros(N_noise, N_radius);
-
+z=1;
 for n=1:N_noise
     coverage_factor = zeros(N_radius,N_grid^2);
     for r = 1:N_radius        
@@ -69,6 +69,7 @@ for n=1:N_noise
 %         end
 %        fprintf('Radius = %d, Coverage factor = %d \n', radius_levels(r), mean(coverage_factor(r,:)));
         load(fname);
+        figure(z);
         subplot(1,2,1);
         imagesc(posterior);
         [fitresult, zfit, fiterr, zerr, resnorm, rr] = fmgaussfit(xvals,yvals,posterior);
@@ -80,12 +81,33 @@ for n=1:N_noise
         posterior_gauss_ang(n,r) = fitresult(2);
         subplot(1,2,2);
         imagesc(zfit);
-
-        pause;
     end 
 end
 
+%% compute coverage 
+N_grid = 50;
+rangeX = [-1 1];
+rangeY = [-1 1];
+xvals = linspace(rangeX(1),rangeX(2),N_grid);
+yvals = linspace(rangeY(1),rangeY(2),N_grid);
 
+for n=1:N_noise
+    coverage_factor = zeros(N_radius,N_grid^2);
+    for r = 1:N_radius 
+        z=1;
+        for x=1:N_grid
+            for y=1:N_grid
+                for i=1:length(RGC_X)            
+                    d = pdist2([xvals(x), yvals(y)], [RGC_X(i), RGC_Y(i)]);
+                    if d <= radius_levels(r)                        
+                        coverage_factor(r,z) = coverage_factor(r,z) + 1;                        
+                    end
+                end
+                z=z+1;
+            end
+        end
+    end
+end
 
 %% make RGC grid
 [RGC_X,RGC_Y] = Hex_grid_generator(3,3);
@@ -106,20 +128,45 @@ RF_params = repmat(params,N_cells,1);
 
 %posterior_noise_5 = posterior_map_for_point(RF_center_points, radii, RF_params, [-1 1], [-1 1], [.25 .5], 40, 10)
 
-%% plot the centers
-scatter(RGC_X, RGC_Y, 50, 'k+');
-ax = gca;
-xlim(ax, [-4 4]);
-ylim(ax, [-4 4]);
+%%
+for r = 1:length(radius_levels)
+    figure(r);
+    % plot the centers
+    scatter(RGC_X, RGC_Y, 50, 'k+');
+    ax(r) = gca;
+    xlim(ax(r), [-4 4]);
+    ylim(ax(r), [-4 4]);
 
-%% make circle rois
-colors = 'rgbcmy';
-for i=1:6
-    RF(i) = drawcircle(ax,'center',[RGC_X(i),RGC_Y(i)],...
-        'color',colors(i),...
-        'FaceAlpha', 0.2, ...
-        'InteractionsAllowed','none',...   
-        'linewidth',1,...
-        'radius',2);
+    % make circle rois
+    colors = 'rgbcmy';
+    for i=1:6
+        RF(i) = drawcircle(ax(r),'center',[RGC_X(i),RGC_Y(i)],...
+            'color',colors(i),...
+            'FaceAlpha', 0.2, ...
+            'InteractionsAllowed','none',...
+            'linewidth',1,...
+            'radius',radius_levels(r));
+    end
+    ch = get(ax(r), 'children');   
+    circles = [];
+    for c=1:length(ch)
+        if strcmp(class(ch(c)), 'images.roi.Circle')
+            circles = [circles, get(ch(c), 'vertices')];
+        end
+    end
+    circles = circles .* factor(r);
+    fname = sprintf('Circles_radius_%d.txt', radius_levels(r));
+    dlmwrite(fname, circles);
+end
+
+%% make RF examples with different SNR
+
+f = fspecial('gaussian',[600, 600], 100);
+f_noise = zeros(600, 600, N_noise);
+for n=1:N_noise
+    f_noise(:,:,n) = f + random('Normal',0,noise_levels(n)*noise_scale,[600 600]);
+    %imagesc(f_noise(:,:,n));
+    fname = sprintf('RF_noise_%d.txt', n);
+    dlmwrite(fname, f_noise(:,:,n));
 end
 
